b056068837c6193a86a5386d4ee1c5fda8b711f9
Gabriel Moyar <gabriel.moyar02@gmail.com>
2023-10-06 03:12:19 -0600

    base table functionality established

--- excercise/src/App.vue
+++ excercise/src/App.vue
@@ -1,22 +1,78 @@
 <template>
   <v-app>
-   <h1>Display Results Here <em>(this can be removed)</em></h1>
+    <v-container>
+      <v-card>
+        <v-card-title>
+          <div>
+            <h2>Scorecard</h2>
+          </div>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="shots"
+          :loading = "isLoading"
+          :items-per-page="15"
+          class="elevation-1"
+          loading-text="Loading... Please wait"
+        ></v-data-table>
+      </v-card>
+    </v-container>
   </v-app>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
+  computed: {
+    ...mapGetters('shots', ['shotList']),
+  },
+  methods: {
+    ...mapActions('shots', ['allShots']),
+
+    modifyShotData() {
+      this.shots.forEach(shot => {
+        shot.courseHoleIndex = shot.courseHoleIndex + 1;
+        shot.shotIndex = shot.shotIndex + 1;
+        shot.totalDistance = shot.totalDistance / 3;
+        shot.totalDistance = shot.totalDistance.toFixed(2);;
+        shot.carryDistance = shot.carryDistance / 3;
+        shot.carryDistance = shot.carryDistance.toFixed(2);
+        shot.launchBallSpeed = shot.launchBallSpeed.toFixed(2);
+        shot.launchBackSpin = shot.launchBackSpin.toFixed(2);
+        shot.launchAngle = shot.launchAngle.toFixed(2);
+      });
+    }
+
+  },
   data() {
     return {
+      headers: [
+        { text: 'Hole', value: 'courseHoleIndex' },
+        { text: 'Shot', value: 'shotIndex' },
+        { text: 'Club Type', value: 'clubTypeKey' },
+        { text: 'Distance (yds)', value: 'totalDistance' },
+        { text: 'Carry (yds)', value: 'carryDistance' },
+        { text: 'Ball Speed (rpm)', value: 'launchBallSpeed' },
+        { text: 'Back Spin (rpm)', value: 'launchBackSpin' },
+        { text: 'Launch Angle °', value: 'launchAngle' }
+      ],
       shotData: {},
-      shots: []
+      shots: [],
+      isLoading: true,
     }
   },
-
   mounted () {
-    //* Sample Vuex call to get shot data.
-    //* You can alter this as well if you see a better way to handle this action.
-    // this.$store.dispatch('shots/allShots');
+    this.allShots()
+      .then(() => {
+        this.shots = this.shotList;
+        this.modifyShotData();
+        this.isLoading = false;
+      })
+      .catch((error) => {
+        console.error('Error loading data:', error);
+        this.isLoading = false;
+      });
   },
 };
 </script>
---
+++ excercise/src/components/Scorecard.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- <v-card>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :search="search"
+      ></v-data-table>
+    </v-card> -->
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
